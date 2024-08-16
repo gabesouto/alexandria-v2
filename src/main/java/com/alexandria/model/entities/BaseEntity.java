bd38317f4fe58ea5efa9bcf930d3908c2baa2b1f
gabesouto <soutogabriel04@gmail.com>
2024-08-16 17:29:39 -0300

    feat: BaseEntity created

---
+++ src/main/java/com/alexandria/model/entities/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.alexandria.model.entities;
+
+import jakarta.persistence.Version;
+import jakarta.persistence.*;
+import java.io.*;
+import java.util.*;
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.*;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity<ID> implements Serializable {
+
+  @Version
+  @Column(nullable = false)
+  protected Integer version;
+  @Column
+  @CreatedBy
+  protected String createdBy;
+  @Column
+  @Temporal(TemporalType.TIMESTAMP)
+//    @CreatedDate
+  protected Date createdAt;
+  @Column
+  @LastModifiedBy
+  protected String updatedBy;
+  @Column
+  @Temporal(TemporalType.TIMESTAMP)
+  @LastModifiedDate
+  protected Date updatedAt;
+
+  public abstract ID getId();
+
+
+  @PrePersist
+  @PreUpdate
+  void onCreate() {
+    if (createdAt == null) {
+      setCreatedAt(new Date());
+    }
+  }
+}
