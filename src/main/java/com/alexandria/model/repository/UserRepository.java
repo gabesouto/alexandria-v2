de948fb2f6403e1b93353dae243f922d60de60c7
gabesouto <soutogabriel04@gmail.com>
2024-09-13 11:44:02 -0300

    perf(UserRepository): add findByEmail query function

--- src/main/java/com/alexandria/model/repository/UserRepository.java
+++ src/main/java/com/alexandria/model/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.alexandria.model.repository;
 
-import com.alexandria.model.entity.*;
+import com.alexandria.model.entity.User;
 import java.util.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-  // Custom methods can be defined here
+
+  UserDetails findByEmail(String login);
 }
