0ba04263b2ddeac4aa7e8a280e2b70fa60f35d73
gabesouto <soutogabriel04@gmail.com>
2024-09-13 11:42:47 -0300

    feat: add role column to User entity and extends UserDetails

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -43,7 +43,9 @@ CREATE TABLE users (
                        full_name VARCHAR(255) NOT NULL,
                        email VARCHAR(255) NOT NULL UNIQUE,
                        password VARCHAR(255) NOT NULL,
-                       username VARCHAR(255) NOT NULL UNIQUE
+                       username VARCHAR(255) NOT NULL UNIQUE,
+                       role VARCHAR(255) NOT NULL,
+
 );
 
 -- Add auditing columns to all tables
