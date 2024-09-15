40beea748c718d9b5ce0058fe2fbe3b094c2377d
gabesouto <soutogabriel04@gmail.com>
2024-09-15 17:17:53 -0300

    feat(SecurityConfiguration): add authentication to routes

--- src/main/java/com/alexandria/infra/security/SecurityConfiguration.java
+++ src/main/java/com/alexandria/infra/security/SecurityConfiguration.java
@@ -1,16 +1,35 @@
 package com.alexandria.infra.security;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.http.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.configuration.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.config.annotation.web.configurers.*;
 import org.springframework.security.config.http.*;
+import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.web.*;
+import org.springframework.security.web.authentication.*;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+  @Autowired
+  private SecurityFilter securityFilter;
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+      throws Exception {
+    return configuration.getAuthenticationManager();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -19,9 +38,13 @@ public class SecurityConfiguration {
         .sessionManagement(session -> session.sessionCreationPolicy(
             SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
-            authorize -> authorize.requestMatchers(HttpMethod.POST, "/books").hasRole("USER")
-                .anyRequest().permitAll()
+            authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                .requestMatchers(HttpMethod.POST, "/books").hasRole("USER")
+                .anyRequest().authenticated()
         )
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 }
